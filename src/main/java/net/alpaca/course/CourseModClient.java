58f311466cb3799a3c8abe00d762d3d8cea8b820
Aless00san <mumaitronzx@gmail.com>
2024-08-03 23:07:35 +0200

    first commit

---
+++ src/main/java/net/alpaca/course/CourseModClient.java
@@ -0,0 +1,10 @@
+package net.alpaca.course;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CourseModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
